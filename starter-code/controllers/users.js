eb86d21b7202bc4e45e0d55098e7d3467b890bc9
w-smith <wsmith@bex.net>
2018-01-09 16:06:44 -0700

    adding it all

--- starter-code/controllers/users.js
+++ starter-code/controllers/users.js
@@ -1,28 +1,48 @@
-var passport = require("passport")
+var passport = require("passport");
 
 // GET /signup
-function getSignup(request, response) {
-}
+function getSignup(request, response, next) {
+    response.render('signup.ejs', { message: request.flash('signupMessage') });
+  }
 
 // POST /signup
-function postSignup(request, response) {
+	  function postSignup(request, response, next) {
+    var signupStrategy = passport.authenticate('local-signup', {
+      successRedirect : '/',
+      failureRedirect : '/signup',
+      failureFlash : true
+    });
+
+    return signupStrategy(request, response, next);
 }
 
 // GET /login
-function getLogin(request, response) { 
-}
+function getLogin(request, response, next) {
+    response.render('login.ejs', { message: request.flash('loginMessage') });
+  }
 
 // POST /login 
-function postLogin(request, response) {
-}
+function postLogin(request, response, next) {
+    var loginStrategy = passport.authenticate('local-login', {
+      successRedirect : '/',
+      failureRedirect : '/login',
+      failureFlash : true
+    });
+
+    return loginStrategy(request, response, next);
+  }
 
 // GET /logout
-function getLogout(request, response) {
+function getLogout(request, response, next) {
+  request.logout();
+  response.redirect('/');
 }
 
 // Restricted page
 function secret(request, response){
+	response.render("secret.ejs");
 }
+
 
 module.exports = {
   getLogin: getLogin,
@@ -31,4 +51,4 @@ module.exports = {
   postSignup: postSignup,
   getLogout: getLogout,
   secret: secret
-}
+};
